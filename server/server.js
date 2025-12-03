8c007e437926f8b112288a4c6003fa6813f08c29
Arjun Singh Yadav <arjunsinghyadav1001@gmail.com>
2025-12-03 10:36:36 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/mongodb.js';
+import userRouter from './routes/userRoutes.js';
+
+const PORT=process.env.PORT||4000;
+const app=express();
+await connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=>res.send("api working"));
+app.use('/api/user',userRouter);
+
+app.listen(PORT,()=>console.log("server running on port "+PORT));
